28a8c155e932758049e1eeccd212e0c2e1697cc9
vedikaS-byte <vedika.shirtekar@gmail.com>
2025-08-05 14:51:39 -0700

    added graphing example

--- partial-practice.R
+++ partial-practice.R
@@ -1,1 +1,45 @@
 ##########
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            --
+##----------------------- PARTIAL DERIVATIVE PRACTICE---------------------------
+##                                                                            --
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#rm(list = ls())
+
+library(tidyverse)
+eq1 <- function(x){
+  3 * x^2 + 4
+  
+}
+
+#create values to evaluate function over
+my_df_range <- data.frame(x = c(1, 100))
+
+#plot as continuous curve over specific range using geom_function()
+my_df_range %>% ggplot(aes(x = x)) + geom_function(fun = eq1) + theme_bw()
+
+#..................example plotting derivatives..................
+ct <- function(t){
+  t^3
+}
+
+#create values to evaluate function over
+my_df_range_2 <- data.frame(t = c(-5, 5))
+
+#plot as continuous curve over specific range using geom_function()
+my_df_range_2 %>% ggplot(aes(x = t)) + geom_function(fun = ct) + theme_bw()
+
+## find derivative
+d_ct <- D(expr = expression(t^3), name = "t")
+d_ct
+
+dc_dt_fun <- function(t){
+  3* t^2
+}
+
+my_df_range_2 %>% ggplot(aes(x = t)) + geom_function(fun = ct, col = "darkred") + 
+  geom_function(fun = dc_dt_fun, col = "darkblue")+ theme_bw()
+
+
+
