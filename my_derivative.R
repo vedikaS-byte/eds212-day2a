f3310d4cd1d3319bd8494bfbd2387fe3f0395463
vedikaS-byte <vedika.shirtekar@gmail.com>
2025-08-05 12:05:30 -0700

    Initial commit

---
+++ my_derivative.R
@@ -0,0 +1,25 @@
+####################
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                            Derivative practice                           ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# create an expression 
+my_expression <- expression(5*x^2)
+my_dev <- D(exp= my_expression, name = "x")
+
+# evaluate derivative at x = 2.8
+x = 2.8
+eval(my_dev)
+
+
+my_derivative <- D(exp = expression(2*z^3 - 10.5*z^2 + 4.1), name = "z")
+my_derivative
+
+my_derivative_2<- D(exp = expression((2*y^3 + 1)^4 - 8*y^3), name = "y")
+my_derivative_2
+?D()
+
+
+
+
+
